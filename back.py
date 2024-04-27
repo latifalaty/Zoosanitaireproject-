bf813c5c866c7efd507e7105934ceedefff190a8
Latifa Jaafari <94934515+latifalaty@users.noreply.github.com>
2024-04-27 23:01:57 +0100

    resultat au bd et filtrage

--- back.py
+++ back.py
@@ -1,93 +1,166 @@
-
 from newspaper import Article
 import re
 import spacy
-from spacy.tokens import DocBin
+from bs4 import BeautifulSoup
+import requests
+import time
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from langdetect import detect
 from transformers import pipeline
 
-import nltk
-from rake_nltk import Rake
-"""# web scrupping"""
+# Initialisation des pipelines de traduction
+translator = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-en")
+translator_ar_to_en = pipeline("translation", model="Helsinki-NLP/opus-mt-ar-en")
 
-def webscraping(urls):
-    articles_content = []  # Liste pour stocker le contenu nettoyé des articles
-    for url in urls:
-        try:
-            # Création de l'objet Article
-            article = Article(url)
+def webscraping(url):
+    try:
+        session = requests.Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
 
-            # Téléchargement et extraction du contenu de l'article
-            article.download()
-            article.parse()
-            content = article.title + ' ' + article.text
-            articles_content.append(content)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
 
-        except Exception as e:
-            print(f"Une erreur s'est produite lors de l'extraction de l'article à l'URL {url}: {e}")
+        article = Article(url)
+        response = session.get(url, headers=headers)
+        time.sleep(5)
 
-    return articles_content
+        article.download(input_html=response.text)
+        article.parse()
+        contenu = article.title + ' ' + article.text
+    except requests.exceptions.RequestException as e:
+        print(f"Une erreur de requête s'est produite lors de l'extraction de l'article à l'URL {url}: {e}")
+        return None, None
+    except Exception as e:
+        print(f"Une erreur inattendue s'est produite lors de l'extraction de l'article à l'URL {url}: {e}")
+        return None, None
 
-"""# nettoyer le text"""
+    date = article.publish_date
+
+    if date is None:
+        soup = BeautifulSoup(response.content, "html.parser")
+        datetime_spans = soup.find_all("span", class_=lambda c: c and "date" in c)
+        for span in datetime_spans:
+            date = span.text.strip()
+            break
+
+    return date, contenu
 
 def nettoyer_paragraphe(paragraphe):
-
     paragraphe_propre = re.sub(r'\s+', ' ', paragraphe)
-
-    # Enlever la ponctuation
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    punc = '''!()-[]{};:'’"\,<>./?@#$%^&*_~'''
     for ele in punc:
         paragraphe_propre = paragraphe_propre.replace(ele, " ")
-
     return paragraphe_propre
 
-"""#importer le modele en local"""
+def detect_language(text):
+    return detect(text)
 
-import zipfile
-import io
+def translate_fr(text):
+    text_morceaux = [text[i:i+512] for i in range(0, len(text), 512)]
+    resultats = []
+    for morceau in text_morceaux:
+        resultat = translator(morceau)
+        resultats.append(resultat)
+    resultat_final = " ".join([resultat[0]['translation_text'] for resultat in resultats])
+    return resultat_final
 
-"""#extraire les maladies"""
+def translate_arabe(text):
+    text_morceaux = [text[i:i+512] for i in range(0, len(text), 512)]
+    resultats = []
+    for morceau in text_morceaux:
+        resultat = translator_ar_to_en(morceau)
+        resultats.append(resultat)
+    resultat_final = " ".join([resultat[0]['translation_text'] for resultat in resultats])
+    return resultat_final
 
-def extraire_maladie(text):
-    nlp1 = spacy.load("model anglais\model-best")
+def extraire_maladie_ang(text):
+    nlp1 = spacy.load("D:/zoosaintaireplatform/model anglais/model-best")
     doc = nlp1(text)
-    maladie = set()
+    maladies = set()
     for ent in doc.ents:
         if ent.label_ == "DISEASE":
-             maladie.add(ent.text.lower())
-    print(maladie)
-    return (maladie)
+             maladie = ent.text.lower()  # Convertir en minuscules pour normaliser
+            # Vérifier si la maladie n'a pas déjà été ajoutée ou une version similaire
+             if maladie not in maladies:
+                 maladies.add(maladie)
+    return(maladies)
 
-"""#resumer de l article"""
+def extraire_maladie_fr(text):
+    nlp1 = spacy.load("D:/zoosaintaireplatform/modelfrancais2/model-best")
+    doc = nlp1(text)
+    maladies = set()
+    for ent in doc.ents:
+        if ent.label_ == "DISEASE":
+             maladie = ent.text.lower()  # Convertir en minuscules pour normaliser
+            # Vérifier si la maladie n'a pas déjà été ajoutée ou une version similaire
+             if maladie not in maladies :
+                maladies.add(maladie)
+    return(maladies)
 
-def summarization(text) :
-  summarizer = pipeline('summarization')
-  resumer=summarizer(text,max_length=300,min_length=50,do_sample=False)
-  print(resumer)
-  return (resumer)
+def extraire_maladie_ar(text):
+    nlp1 = spacy.load("D:/zoosaintaireplatform/modelar/model-best")
+    doc = nlp1(text)
+    maladies = set()
+    mots_specifiques = ("السل", "الجدري", "البروسيلا")
+    for ent in doc.ents:
+        if ent.label_ == "DISEASE":
+            maladie = ent.text
+            # Vérifier si la maladie n'a pas déjà été ajoutée ou une version similaire
+            if maladie not in maladies and (len(maladie.split()) > 1 or maladie in mots_specifiques):
+                maladies.add(maladie)
+    return maladies
+
+def extract_country_ang(text):
+    text = text.title()
+    nlp = spacy.load("en_core_web_lg")
+    doc = nlp(text)
+    countries = []
+    for ent in doc.ents:
+        if (ent.label_ == "LOC_B" or ent.label_ == "GPE") and ent.text not in countries:
+            countries.append(ent.text)
+    return countries
+
+def summarize_article(article_text, sentences_count=3):
+    parser = PlaintextParser.from_string(article_text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, sentences_count)
+    return " ".join(str(sentence) for sentence in summary)
 
 """#main
 
 """
 
-urls=["https://equusmagazine.com/news/eq-edcc-health-watch/four-texas-horses-positive-for-eia/","https://www.mlive.com/news/saginaw-bay-city/2023/11/avian-influenza-detected-in-bay-county.html"]
-contenu= webscraping(urls)
-print(contenu)
-for paragraphe in contenu:
-    contenu_nettoye = nettoyer_paragraphe(paragraphe)
-    print(contenu_nettoye)
-    nlp1 = spacy.load("model anglais\model-best")
-    resumer = summarization(paragraphe)
-    maladies = extraire_maladie(contenu_nettoye)
+urls = ["https://akhbarelyom.com/news/newdetails/4309637/1/%D8%AA%D8%AD%D8%B5%D9%8A%D9%86-51-%D9%85%D9%86-%D8%A7%D9%84%D9%85%D8%A7%D8%B4%D9%8A%D8%A9-%D8%B6%D8%AF-%D9%85%D8%B1%D8%B6-%D8%A7%D9%84%D8%AC%D9%84%D8%AF-%D8%A7%D9%84%D8%B9%D9%82%D8%AF%D9%8A-"]
+for url in urls:
+    (date, contenu) = webscraping(url)
+    print(contenu)
+    if contenu is not None:
+        if detect_language(contenu) == "fr":
+          contenu_ang = translate_fr(contenu)
+          maladies = extraire_maladie_fr(nettoyer_paragraphe(contenu))
+        elif detect_language(contenu) == "ar":
+          contenu_ang = translate_arabe(contenu)
+          maladies = extraire_maladie_ar(nettoyer_paragraphe(contenu))
+        elif detect_language(contenu) == "en":
+          contenu_ang=contenu
+          maladies = extraire_maladie_ang(nettoyer_paragraphe(contenu_ang))
+        else:
+          print("Langue non supportée")
+          break
 
-"""#keywords
+        contenu_nettoye = nettoyer_paragraphe(contenu_ang)
 
-"""
-
-nltk.download('stopwords')
-nltk.download('punkt')
-text="Four horses in Texas have been confirmed positive for equine infectious anemia . The cases are located in Waller, Montgomery, Denton and El Paso counties . The Texas Animal Health Commission is working closely with the owners and local veterinarians to monitor potentially exposed horses ."
-r = Rake()  # Set max_length to 2 for one or two-word phrases
-r.extract_keywords_from_text(text)
-for rating, keyword in r.get_ranked_phrases_with_scores():
-    if rating > 5:
-        print(keyword,rating)
+        summary=summarize_article(contenu, sentences_count=4)
+        pays = extract_country_ang(contenu_nettoye)
+        print("Date de publication:", date)
+        print("Résumé:", summary)
+        print("Maladies:", maladies)
+        print("Pays:", pays)
--- interface.py
+++ interface.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session, make_response, jsonify
 from pymongo import MongoClient
 from bson import ObjectId
-from back import webscraping, nettoyer_paragraphe, extraire_maladie, summarization
 import pandas as pd
+
+from back import webscraping, nettoyer_paragraphe
+from back import detect_language, translate_fr, translate_arabe, summarize_article, extract_country_ang,extraire_maladie_fr,extraire_maladie_ar
+from back import extraire_maladie_ang
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
@@ -10,6 +14,7 @@ app.secret_key = 'your_secret_key'
 client = MongoClient('mongodb://localhost:27017/')
 db = client['zoo']
 collection = db['user']
+extracteddata = db['extracteddata']
 
 # Nom d'utilisateur et mot de passe de l'administrateur
 admin_username = 'admin'
@@ -17,10 +22,7 @@ admin_password = 'adminpassword'
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        users = collection.find()
-        return render_template('index.html', username=session['username'], users=users)
-    return redirect(url_for('login'))
+     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -56,33 +58,48 @@ def add_user():
     email = request.form['email']
     user_data = {'username': username, 'password': password, 'email': email}
     collection.insert_one(user_data)
-    return redirect(url_for('index'))
+    return redirect(url_for('listusers'))
 
 @app.route('/delete_user/<user_id>', methods=['POST'])
 def delete_user(user_id):
     # Supprimer un utilisateur de la collection en utilisant son ID
     collection.delete_one({'_id': ObjectId(user_id)})
-    return redirect(url_for('index'))
-@app.route('/filter', methods=['POST'])
-def filter_data():
-    filter_criteria = request.json 
-    
-    country = filter_criteria.get('country')
-    disease = filter_criteria.get('disease')
-    symptom = filter_criteria.get('symptom')
+    return redirect(url_for('listusers'))
+from datetime import datetime
 
-    query = {}
-    if country:
-        query['country'] = country
-    if disease:
-        query['disease'] = disease
-    if symptom:
-        query['symptom'] = symptom
+@app.route('/filter_by_disease', methods=['POST'])
+def filter_by_disease():
+    # Récupérer le nom de la maladie à filtrer depuis le formulaire POST
+    disease_name = request.form.get('disease')
 
-    filtered_data = collection.find(query)
+    # Rechercher les données dans la base de données qui contiennent la maladie spécifiée
+    filtered_data = extracteddata.find({'maladies': disease_name})
+
+    # Convertir les résultats en une liste de dictionnaires
+    filtered_data_list = list(filtered_data)
+
+    return render_template('filtered_data.html', filtered_data=filtered_data_list)
 
 
-    return jsonify({'result': list(filtered_data)})
+@app.route('/filter_by_date', methods=['POST'])
+def filter_by_date():
+    # Récupérer les dates de début et de fin pour la période de filtrage depuis le formulaire POST
+    start_date_str = request.form.get('start_date')
+    end_date_str = request.form.get('end_date')
+
+    # Convertir les dates de chaînes de caractères en objets datetime
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+
+    # Rechercher les données dans la base de données qui sont dans la plage de dates spécifiée
+    filtered_data = extracteddata.find({'date': {'$gte': start_date, '$lte': end_date}})
+
+    # Convertir les résultats en une liste de dictionnaires
+    filtered_data_list = list(filtered_data)
+
+    return render_template('filtered_data.html', filtered_data=filtered_data_list)
+
+
 @app.route('/userinterface')
 def userinterface():
     return render_template('userinterface.html')
@@ -92,33 +109,70 @@ def scrape():
     # Obtenez les URL à partir du formulaire
     urls = request.form.getlist('url')
     
-    # Effectuez le web scraping
-    contenu = webscraping(urls)
-    
-    # Nettoyez le texte et extrayez les maladies pour chaque paragraphe
-    maladies = []
-    resumers = []  # Liste pour stocker les résumés de chaque paragraphe
-    
-    
-    for paragraphe in contenu:
-        paragraphe_propre = nettoyer_paragraphe(paragraphe)
-        maladies_article = extraire_maladie(paragraphe_propre)
-        maladies.append(maladies_article)
-        resumer = summarization(paragraphe_propre)
-        resumers.append(resumer)  # Ajoutez le résumé à la liste
-    
-    # Vous pouvez maintenant utiliser les données (contenu, maladies, résumé) comme vous le souhaitez
-    return render_template('result.html', contenu=contenu, maladies=maladies, resumers=resumers)
+    # Liste pour stocker les données extraites
+    extracted_data = []
 
+    for url in urls:
+        # Effectuez le web scraping
+        (date, contenu) = webscraping(url)
+
+        if contenu is not None:
+            # Détection de la langue et traitement en conséquence
+            if detect_language(contenu) == "fr":
+                contenu_ang = translate_fr(contenu)
+                maladies = extraire_maladie_fr(nettoyer_paragraphe(contenu))
+            elif detect_language(contenu) == "ar":
+                contenu_ang = translate_arabe(contenu)
+                maladies = extraire_maladie_ar(nettoyer_paragraphe(contenu))
+            elif detect_language(contenu) == "en":
+                contenu_ang = contenu
+                maladies = extraire_maladie_ang(nettoyer_paragraphe(contenu_ang))
+            else:
+                print("Langue non supportée")
+                continue
+
+            # Nettoyage du contenu
+            contenu_nettoye = nettoyer_paragraphe(contenu_ang)
+
+            # Résumé de l'article
+            summary = summarize_article(contenu, sentences_count=4)
+
+            # Extraction des pays
+            pays = extract_country_ang(contenu_nettoye)
+
+            # Stockage des données extraites dans un dictionnaire
+            data = {
+                'url': url,
+                'date': date,
+                'summary': summary,
+                'maladies': list(maladies),
+                'pays': pays
+            }
+            extracteddata.insert_one(data)
+            # Ajout du dictionnaire à la liste
+            extracted_data.append(data)
+
+    # Vous pouvez maintenant utiliser extracted_data comme vous le souhaitez
+    return render_template('result.html', extracted_data=extracted_data)
 @app.route('/export_to_excel', methods=['POST'])
 def export_to_excel():
-    contenu = request.form['contenu']
-    maladies = request.form.getlist('maladie')
-    resumes = request.form.getlist('resumer')
+    # Récupérer les données depuis la base de données extracteddata
+    extracted_data = extracteddata.find()
+
+    # Créer une liste de dictionnaires avec les données extraites
+    data_list = []
+    for data in extracted_data:
+        data_dict = {
+            'Maladies': ', '.join(data['maladies']) if 'maladies' in data else '',
+            'Résumés': data['summary'] if 'summary' in data else '',
+            'URL': data['url'] if 'url' in data else '',
+            'Date': data['date'] if 'date' in data else '',
+            'Pays': ', '.join(data['pays']) if 'pays' in data else ''
+        }
+        data_list.append(data_dict)
 
     # Créer un DataFrame avec les données
-    data = {'Contenu': [contenu], 'Maladies': maladies, 'Résumés': resumes}
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(data_list)
 
     # Exporter vers un fichier Excel
     excel_file = 'exported_data.xlsx'
@@ -131,6 +185,15 @@ def export_to_excel():
 
     return response
 
+@app.route('/add_user_form')
+def add_user_form():
+    return render_template('add_user_form.html')
+@app.route('/listusers')
+def listusers():
+    if 'username' in session:
+        users = collection.find()
+    return render_template('listeusers.html', username=session['username'], users=users)
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
